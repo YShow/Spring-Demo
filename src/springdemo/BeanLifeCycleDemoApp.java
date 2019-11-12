648c404564f377d2ee51ea4df82d918ac099da01
yasser <yassermbb@gmail.com>
2019-11-12 15:06:47 -0200

    try with-resource

--- src/springdemo/BeanLifeCycleDemoApp.java
+++ src/springdemo/BeanLifeCycleDemoApp.java
@@ -5,11 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BeanLifeCycleDemoApp {
 	public static void main(String[] args) {
 		var context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
-		
+		try(context)
+		{
 		var theCoach = context.getBean("myCoach",Coach.class);
 
 		System.out.println(theCoach.getDailyWorkout());
 		
-		context.close();
+		}
 	}
 }

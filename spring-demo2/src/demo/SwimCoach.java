46a45807d1c191749d6dee09aac1244376646182
yasser <yassermbb@gmail.com>
2019-11-13 12:27:46 -0200

    Field injection funcionando.

--- spring-demo2/src/demo/SwimCoach.java
+++ spring-demo2/src/demo/SwimCoach.java
@@ -31,4 +31,12 @@ public class SwimCoach implements Coach {
 		return fortuneService.getFortune();
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
 }
--- spring-demo2/src/demo/SwimJavaConfigDemoApp.java
+++ spring-demo2/src/demo/SwimJavaConfigDemoApp.java
@@ -7,11 +7,15 @@ public class SwimJavaConfigDemoApp {
 	public static void main(final String[] args) {
 		final var context = new AnnotationConfigApplicationContext(SportConfig.class);
 		try (context) {
-			final var theCoach = context.getBean("swimCoach", Coach.class);
+			final var theCoach = context.getBean("swimCoach", SwimCoach.class);
 
 			System.out.println(theCoach.getDailyWorkout());
 
 			System.out.println(theCoach.getDailyFortune());
+
+			System.out.println(theCoach.getEmail());
+
+			System.out.println(theCoach.getTeam());
 		}
 	}
 
